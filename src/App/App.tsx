21787afd6993bb259d5ad95c8585143d2a52c520
James England <me@jamesrengland.com>
2025-01-08 16:34:26 -0600

    added a DEV/PROD flag so that the live URL doesn't just display broken code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -10,6 +10,7 @@ import './App.css';
 const App = () => {
   const { selectedPlaylist } = useAppState();
   const { isLoggedIn, handleLogin, refreshAppToken, refreshSpotifyToken } = useAuthState();
+  const configMode:'DEV'|'PROD' = import.meta.env.VITE_CONFIG;
 
   useEffect(() => {
 	// const token = localStorage.getItem('spotify_access_token');
@@ -58,48 +59,57 @@ const App = () => {
 
   return (
 	<div>
-	  <h1>Welcome to Songelo!</h1>
-	  <p>This is the home page of the app.</p>
-	  {isLoggedIn ? (
-		
-		<div className="logged-in-user-container">
+		{configMode === 'DEV' ? (
+			<>
+				<h1>Welcome to Songelo!</h1>
+				<p>This is the home page of the app.</p>
+				{isLoggedIn ? (
+				
+				<div className="logged-in-user-container">
 
-		  <div className="headerContent">
-			<p>You are logged in with Spotify!</p>
+					<div className="headerContent">
+					<p>You are logged in with Spotify!</p>
 
-			<div>
-			  <button onClick={refreshAppToken} style={{ padding: '10px 20px', fontSize: '16px' }}>
-				Refresh App Token
-			  </button>
-			  <button onClick={refreshSpotifyToken} style={{ padding: '10px 20px', fontSize: '16px' }}>
-				Refresh Spotify Token
-			  </button>
-			</div>
-		  </div>
+					<div>
+						<button onClick={refreshAppToken} style={{ padding: '10px 20px', fontSize: '16px' }}>
+						Refresh App Token
+						</button>
+						<button onClick={refreshSpotifyToken} style={{ padding: '10px 20px', fontSize: '16px' }}>
+						Refresh Spotify Token
+						</button>
+					</div>
+					</div>
 
-		  <div className="mainContent">
+					<div className="mainContent">
 
-			<div className="leftPanel">
-				{selectedPlaylist && <TopTracks onSongAdded={() => { /* handle song added */ }} />}
-			</div>
+					<div className="leftPanel">
+						{selectedPlaylist && <TopTracks onSongAdded={() => { /* handle song added */ }} />}
+					</div>
 
-			
-			  <div className="centerContent">
-				<SongeloPlaylistsDropdown />
-				{selectedPlaylist && (
-					<ListSongeloPlaylistSongs playlistId={selectedPlaylist} />
+					
+						<div className="centerContent">
+						<SongeloPlaylistsDropdown />
+						{selectedPlaylist && (
+							<ListSongeloPlaylistSongs playlistId={selectedPlaylist} />
+						)}
+						</div>
+					
+					</div>
+
+					
+				</div>
+				) : (
+				<button onClick={handleLogin} style={{ padding: '10px 20px', fontSize: '16px' }}>
+					Login with Spotify
+				</button>
 				)}
-			  </div>
-			
-		  </div>
-
-		  
-		</div>
-	  ) : (
-		<button onClick={handleLogin} style={{ padding: '10px 20px', fontSize: '16px' }}>
-		  Login with Spotify
-		</button>
-	  )}
+			</>
+		) : (
+			<>
+				<h1>Coming Soon!</h1>
+			</>
+		)}
+	  
 	</div>
   );
 };
