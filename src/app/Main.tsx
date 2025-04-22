88b82d1f96e4f29153db4c8e740c48b4f7ddb614
James England <me@jamesrengland.com>
2025-04-22 11:26:57 -0500

    added privacy policy

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import PrivacyPolicy from '../components/PrivacyPolicy/PrivacyPolicy';
 import Callback from '../components/Callback/Callback';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/playlist/:playlistID/:viewStyle" element={<App />} />
         <Route path="/playlist/:playlistID" element={<App />} />
         <Route path="/callback" element={<Callback />} />
